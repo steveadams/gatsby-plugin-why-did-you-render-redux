1d5eaafed2a7574925ebc369039a2f31c2bb306e
Steve Adams <steve@steve-adams.me>
2020-05-25 15:34:04 -0700

    Use composeWithDevTools to catch edgecases previous solution missed

--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,15 @@
 /* Copyright 2005-present Instant Domain Search, Inc. */
 
-import {combineReducers, compose, createStore as reduxCreateStore, Store} from 'redux';
-
-import {Action} from './actionTypes';
 import * as reducers from './reducers';
+
+import {combineReducers, createStore as reduxCreateStore, Store} from 'redux';
+import {Action} from './actionTypes';
+import {composeWithDevTools} from 'redux-devtools-extension';
 
 export const store = reduxCreateStore<reducers.State, Action, Store<reducers.State, Action>, Record<string, unknown>>(
   combineReducers<reducers.State>(reducers),
   {} as reducers.State,
-  // compose enhancer is defined depending on if it's needed for SSR or HMR
-  typeof window === 'object' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose,
+  composeWithDevTools(),
 );
 
 export const dispatch = (action: Action) => {
