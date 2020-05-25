eb5fbd0b70f700988d0a2127150538a8f74c1ba2
Steve Adams <steve@steve-adams.me>
2020-05-25 12:54:20 -0700

    Define the compose enhancers depending on presence of `window` and redux dev tools
    This fixes an issue where HMR could load __REDUX_DEVTOOLS_EXTENSION_COMPOSE__(), but
    SSR couldn't since window isn't defined. There's an npm library to handle this with
    typings included, but that seems like overkill. The API and types seem stable.

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
 /* Copyright 2005-present Instant Domain Search, Inc. */
 
-import {combineReducers, createStore as reduxCreateStore, Store} from 'redux';
+import {combineReducers, compose, createStore as reduxCreateStore, Store} from 'redux';
 
 import {Action} from './actionTypes';
 import * as reducers from './reducers';
@@ -8,7 +8,8 @@ import * as reducers from './reducers';
 export const store = reduxCreateStore<reducers.State, Action, Store<reducers.State, Action>, Record<string, unknown>>(
   combineReducers<reducers.State>(reducers),
   {} as reducers.State,
-  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  // compose enhancer is defined depending on if it's needed for SSR or HMR
+  typeof window === 'object' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose,
 );
 
 export const dispatch = (action: Action) => {
